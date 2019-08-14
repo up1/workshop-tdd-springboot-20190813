4f92d328bf444aab1f4df675939669ec22b3c251
up1 <somkiat.p@gmail.com>
2019-08-14 15:32:58 +0700

    Integrate repository to service

--- workshop/day2/src/main/java/com/example/day2/todo/TodoService.java
+++ workshop/day2/src/main/java/com/example/day2/todo/TodoService.java
@@ -1,5 +1,6 @@
 package com.example.day2.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,20 @@ import java.util.List;
 @Service
 public class TodoService {
 
-    public List<TaskResponse> inquiryAll() {
-        List<TaskResponse> tasks = new ArrayList<>();
-        tasks.add(new TaskResponse(1, "Task 1"));
-        tasks.add(new TaskResponse(2, "Task 2"));
+    @Autowired
+    private TaskRepository taskRepository;
 
-        return tasks;
+    public List<TaskResponse> inquiryAll() {
+        // Get data from repository
+        List<Task> tasks = (List<Task>) taskRepository.findAll();
+
+        // Map to response
+        List<TaskResponse> results = new ArrayList<>();
+        for (Task task: tasks) {
+            results.add(new TaskResponse(task.getId(), task.getName()));
+        }
+
+        return results;
     }
 
 }
